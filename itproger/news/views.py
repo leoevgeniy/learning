ba05e514f0a20d3a098f8c7532e435bd6ce54b20
YourDax <64884803+YourDax@users.noreply.github.com>
2022-04-24 14:39:22 +0400

    1

--- itproger/news/views.py
+++ itproger/news/views.py
@@ -1,7 +1,7 @@
 from .models import Article
 from django.shortcuts import render, redirect
 from .forms import ArticleForm
-from django.views.generic import DetailView
+from django.views.generic import DetailView, UpdateView, DeleteView
 
 
 def news_home(request):
@@ -12,6 +12,18 @@ class NewsDetailView(DetailView):
     model = Article
     template_name = 'news/details_view.html'
     context_object_name = 'article'
+
+class NewsUpdateView(UpdateView):
+    model = Article
+    template_name = 'news/create.html'
+    form_class = ArticleForm
+
+class NewsDeleteView(DeleteView):
+    model = Article
+    success_url = '/news/'
+    template_name = 'news/news-delete.html'
+
+
 
 def create(request):
     error = ''
